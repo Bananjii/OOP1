c1503b653bbeeb5438d761303db490fa20af81b3
Bananjii <mr.mrsanya@gmail.com>
2024-11-01 21:41:39 +0200

    Vantazhni.cpp

--- OOP1/LR9/Vantazhni.cpp
+++ OOP1/LR9/Vantazhni.cpp
@@ -1,5 +1,6 @@
 #include "Vantazhni.h"
 #include <iostream>
+#include <limits>
 
 Vantazhni::Vantazhni(std::string n, double c, int d, double w)
     : Perevezennya(n, c, d), weight(w) {}
@@ -8,8 +9,19 @@ Vantazhni::~Vantazhni() {}
 
 void Vantazhni::input() {
     Perevezennya::input();
-    std::cout << "Введіть вагу вантажу (тонни): ";
-    std::cin >> weight;
+
+    // Перевірка для введення ваги вантажу
+    while (true) {
+        std::cout << "Введіть вагу вантажу (тонни): ";
+        std::cin >> weight;
+        if (std::cin.fail() || weight <= 0) {
+            std::cout << "Вага вантажу має бути числом більше 0. Спробуйте ще раз.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
 }
 
 void Vantazhni::display() const {

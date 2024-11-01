66ce8ef9ed1dacfe8b4532fae8a5425410aadd53
Bananjii <mr.mrsanya@gmail.com>
2024-11-01 21:39:19 +0200

    Vantazhni.cpp

--- OOP1/LR8/Vantazhni.cpp
+++ OOP1/LR8/Vantazhni.cpp
@@ -1,5 +1,6 @@
 #include "Vantazhni.h"
 #include <iostream>
+#include <limits>
 
 Vantazhni::Vantazhni(std::string n, double c, int d, double w)
     : Perevezennya(n, c, d), weight(w) {}
@@ -17,12 +18,22 @@ void Vantazhni::setWeight(double w) {
         std::cout << "Вага не може бути негативною!" << std::endl;
 }
 
-// Метод для введення даних
+// Метод для введення даних з перевірками
 void Vantazhni::input() {
-    Perevezennya::input(); // Викликаємо базовий метод для вводу загальних даних
-    std::cout << "Введіть вагу вантажу (тонни): ";
-    std::cin >> weight;
-    setWeight(weight); // Викликаємо метод для валідації
+    Perevezennya::input();
+
+    while (true) {
+        std::cout << "Введіть вагу вантажу (тонни): ";
+        std::cin >> weight;
+        if (std::cin.fail() || weight < 0) {
+            std::cout << "Вага повинна бути числом більше 0. Спробуйте ще раз.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            setWeight(weight);
+            break;
+        }
+    }
 }
 
 void Vantazhni::display() const {
